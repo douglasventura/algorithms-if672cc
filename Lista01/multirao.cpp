cd10b787f9728f0d044ea38e53667e16beb0ddd2
Douglas Ventura <dvsf@cin.ufpe.br>
2018-08-28 21:59:11 -0300

    Esqueleto feito. Falta implementar as funcoes e seus respectivos metodos.

---
+++ Lista01/multirao.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+struct node {
+    // build the function and methods
+};
+
+struct queue {
+    // build the function and methods
+};
+
+struct stack {
+    // build the function and methods
+};
+
+int main(int argc, char *argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int e, process, id, p_hours, w_hours;
+    cin >> e;
+
+    for (int i = 0; i < e; i++) {
+        
+    }
+    
+
+    return 0;
+}
