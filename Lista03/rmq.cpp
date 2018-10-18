49b525e81a2da111625f87441876b13f7ef44de3
Douglas Ventura <dvsf@cin.ufpe.br>
2018-10-17 21:16:10 -0300

    Alterei a questao da Binary Tree e estou implementando a  Heap

--- Lista03/rmq.cpp
+++ Lista03/rmq.cpp
@@ -3,6 +3,32 @@
 
 using namespace std;
 
+struct node {
+    int l;
+    int r;
+    int value;
+    node *right;
+    node *left;
+
+    node(int l, int r, int value) : l(l), r(r), value(value), root(nullptr) {}
+};
+
+
+struct binary_tree {
+    node *root;
+
+    binary_tree(int *arr_sensors[], int l, int r) {
+        this->right = nullptr;
+        this->left = nullptr;
+
+        if(l == r) {
+            this->root->l = l;
+            this->root->r = r;
+        } else if () {
+
+        }
+    }
+};
 
 
 int main(int argc, char *argv[]) {
@@ -11,12 +37,14 @@ int main(int argc, char *argv[]) {
 
     int num_sensors, sensors, a, b;
     string event;
-    
+
     cin >> num_sensors;
+    int arr_sensors[num_sensors];
     event = " ";
 
     for (int i = 0; i < num_sensors; ++i) {
-
+        cin >> sensors;
+        arr_sensors[i] = sensors;
     }
 
     while (event != "END") {
--- Lista04/hospital_universitario.cpp
+++ Lista04/hospital_universitario.cpp
@@ -1,47 +1,98 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #define endl '\n'
 
 using namespace std;
 
-struct pacient {
+
+struct patient {
     int age;
     int urgency;
-    int service;
+    int arrival;
 
-    pacient (int age, int urgency, int service) : age(age), urgency(urgency), service(service) {}    
-}
+    patient() {}
+    patient(int age, int urgency, int arrival) : age(age), urgency(urgency), arrival(arrival) {}
 
-class Heap {
-    
+};
 
-}
+
+struct heap {
+    patient *f;
+    int arraySize;
+    int heapSize;
+
+    heap() {
+        this->f = nullptr;
+        this->arraySize = 1;
+        this->heapSize = 0;
+        this->f = new patient[arraySize];
+    }
+
+    void heap_insert(patient *p) {
+        if (this->arraySize == this->heapSize) {
+            double_size();
+        }
+        f[this->heapSize] = *p;
+        bubble_up(heapSize);
+        this->heapSize++;
+    }
+
+    void double_size() {
+
+    }
+
+    void bubble_up(int heapSize) {
+        int i = heapSize;
+        while (i > 0 && this->f[i].urgency >= this->f[(i-1) / 2].urgency) {
+            if ()
+            patient *aux;
+            aux = this->p[i];
+            this->p[i] = p[(i-1) / 2];
+            this->p[(i-1) / 2] = aux;
+            i = ((i-1)/2) - 1;
+        }
+    }
+
+    void heap_remove() {
+
+
+    }
+
+};
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int num_services, age, urgency, service;
+    int numServices, age, urgency, service, index, arrival;
     string command;
+    patient *p;
 
-    cin << num_services;
-    cin << command;
+    command = " ";
+    arrival = 0;
+
+    cin >> numServices;
+
+    heap *services[numServices];
 
     while (command != "END") {
-        cin << command;
-        
+        cin >> command;
+
         if (command == "CIN") {
-            cin << age, urgency, service;
-
-
+            cin >> age >> urgency >> service;
+            p = new patient(age, urgency, arrival);
+            services[service]->heap_insert(p);
+            ++arrival;
 
         } else if (command == "NXT") {
-            
-
+            cin >> index;
+            p = services[index]->nxt();
+            cout << index << p->age << p->urgency << endl;
+            services[index]->heap_remove();
 
         } else if (command == "STD") {
+            // p = services[]->std();
 
-
-
+            cout << 0 << p->age << p->urgency << endl;
         }
     }
 
