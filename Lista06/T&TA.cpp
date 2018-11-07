5b26a1d3e503e1242a566b9892e14db511da8c8f
Douglas Ventura da Silva Ferreira <dvsf@ug4c47.windows.cin.ufpe.br>
2018-11-07 11:49:54 -0300

    Adicionei algumas coisas, mas tem bug no reconhecimento da entrada

--- Lista06/T&TA.cpp
+++ Lista06/T&TA.cpp
@@ -4,9 +4,32 @@
 
 using namespace std;
 
-struct Graph {
+struct NodeWeight { // objeto nó com os pesos.
+    int node, speed, cost;
 
-}
+    NodeWeight(int node, int speed, int cost) : node(node), speed(speed), cost(cost) {}
+
+};
+
+struct Graph {
+    int nodes; 
+    list<NodeWeight> *adjacent;
+
+    Graph(int nodes) {
+        this->nodes = nodes;
+        this->adjacent = new list<NodeWeight>[this->nodes];
+    }
+
+    void addEdge(int nodeX, int nodeY, NodeWeight *x, NodeWeight *y) {
+        this->adjacent[nodeX].push_back(*y);
+        this->adjacent[nodeY].push_back(*x);
+    }
+
+    int* dijkstra(int origin, int destiny) {
+        
+    }
+
+};
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(false);
@@ -14,31 +37,37 @@ int main(int argc, char *argv[]) {
 
     double p;
     int n, m, nodeX, nodeY, speed, cost, origin, destiny;
-    int[] shortest_path;
+    int *shortest_path = new int[1];
 
     cin >> p;
     cin >> n >> m;
 
+    Graph g(n);
+
     for (int i = 0; i < m; ++i) {
         cin >> nodeX >> nodeY >> speed >> cost;
 
+        NodeWeight *x = new NodeWeight(nodeX, speed, cost);
+        NodeWeight *y = new NodeWeight(nodeY, speed, cost);
+
+        g.addEdge(nodeX, nodeY, x, y);
 
     }
 
-    while (cin.hasNextInt()) {
+    while (cin.eof()) {
         cin >> origin >> destiny;
 
-        shortest_path = dijkstra(origin, destiny);
+        shortest_path = g.dijkstra(origin, destiny);
 
-        for (int i = 0; i < shortest_path.size(); ++i) {
+        for (int i = 0; i < sizeof(shortest_path); ++i) {
             cout << shortest_path[i];
 
-            if (i < shortest_path.size() - 1) {
+            if (i < sizeof(shortest_path) - 1) {
                 cout << " ";
             } else {
                 cout << endl;
             }
-        } 
+        }
     }
 
     return 0;
