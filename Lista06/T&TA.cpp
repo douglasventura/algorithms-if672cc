32c7180b89214d064928a78a967982ca1b642ccb
Douglas Ventura <dvsf@cin.ufpe.br>
2018-11-12 00:53:39 -0300

    Falta ajeitar o Dijkstra

--- Lista06/T&TA.cpp
+++ Lista06/T&TA.cpp
@@ -6,11 +6,86 @@ using namespace std;
 
 struct NodeWeight { // objeto nó com o peso.
     int link; // ligacao com o nó
-    int weight; // peso do nó
+    double weight; // peso do nó
 
-    NodeWeight(int link, int weight) : link(link), weight(weight) {}
+    NodeWeight(int link, double weight) : link(link), weight(weight) {}
 
 };
+
+
+struct MinHeap {
+    NodeWeight **f;
+    int arraySize;
+    int heapSize;
+
+    MinHeap() {
+        this->f = new NodeWeight*[2];
+        this->arraySize = 2;
+        this->heapSize = 0;
+    }
+
+    void heap_insert(NodeWeight *n) {
+        if (this->arraySize == this->heapSize) {
+            double_size();
+        }
+        f[this->heapSize] = n;
+        this->heapSize++;
+        bubble_up(heapSize);
+    }
+
+    void double_size() {
+        NodeWeight **aux = new NodeWeight*[2* this->arraySize];
+        for (int i = 0; i < this->arraySize; ++i) {
+            aux[i] = this->f[i];
+        }
+        this->f = aux;
+        this->arraySize = 2* this->arraySize;
+    }
+
+    void bubble_up(int heapSize) {
+        int i = heapSize - 1;
+
+        while (i > 0 && this->f[i]->weight < this->f[(i-1)/2]->weight) {
+            NodeWeight *aux2 = this->f[i];
+            this->f[i] = f[(i-1)/2];
+            this->f[(i-1)/2] = aux2;
+            i = (i-1)/2;
+        }
+    }
+
+    NodeWeight* heap_extract() {
+        NodeWeight *aux3 = this->f[0];
+        this->f[0] = this->f[this->heapSize - 1];
+        this->f[this->heapSize - 1] = nullptr;
+        this->heapSize--;
+        heapify(0);
+
+        return aux3;
+    }
+
+    void heapify(int i) {
+        int m = i;
+        int l = (2 * i) + 1;
+        int r = (2 * i) + 2;
+
+        if (l < this->heapSize && this->f[l]->weight <= this->f[m]->weight) {
+            m = l;
+        }
+
+        if (r < this->heapSize && this->f[r]->weight <= this->f[m]->weight) {
+            m = r;
+        }
+
+        if (m != i) {
+            NodeWeight *aux4 = this->f[i];
+            this->f[i] = this->f[m];
+            this->f[m] = aux4;
+            heapify(m);
+        }
+
+    }
+};
+
 
 struct Graph {
     int nodes; // número de nós
@@ -22,39 +97,60 @@ struct Graph {
     }
 
     void addEdge(int nodeX, int nodeY, NodeWeight *x, NodeWeight *y) { // adiciona a conexão entre os vértices
-        this->adjacent[nodeX].push_back(*y);
-        this->adjacent[nodeY].push_back(*x);
+        this->adjacent[nodeX].push_back(*x);
+        this->adjacent[nodeY].push_back(*y);
     }
 
     int* dijkstra(int origin, int destiny) {
-        int dist[this->nodes]; // vetor com as distâncias entre os vértices
-        int visited[this->nodes]; // vetor de vértices visitados
+        double weights[this->nodes]; // vetor com os pesos entre os vértices
+        int *precursor = new int[this->nodes]; // vetor com os precusores do menor caminho
+        bool visited[this->nodes]; // vetor de vértices visitados
+
 
         for(int i = 0; i < this->nodes; ++i) {
-           dist[i] = INT_MAX; // atribuindo infinito (maior valor inteiro) as distâncias
-           visited[i] = false; // marcando todos os vértices como não visitados
+            weights[i] = INT_MAX;
+            precursor[i] = -1;
+            visited[i] = false;
         }
 
-        dist[origin] = 0; // a distância do vértice de origem para ele mesmo é 0
+        weights[origin] = 0;
+        precursor[0] = origin;
 
+        MinHeap *h = new MinHeap();
+        NodeWeight *aux = new NodeWeight(origin, 0);
 
-        // inserir na MinHeap aqui;
+        h->heap_insert(aux);
 
-        while() { // enquanto tiver elemento na MinHeap
-            // extrair o vértice e o peso do topo da MinHeap
-            // obter o vértice
-            // remover da fila
+        int index = 1;
 
-            int u;
+        while(h) { // enquanto tiver elemento na MinHeap
+            aux = h->heap_extract();
+            int u = aux->link;
 
             if(!visited[u]) {
-                visited[u] = false;
+                visited[u] = true;
 
-                // percorrer a lista com um interator
+                list<NodeWeight>::iterator it;
 
-                // percorrer os vértices adjacentes de u
+                for (it = this->adjacent[u].begin(); it != this->adjacent[u].end() && !this->adjacent[u].empty(); it++) {
+                    int v = it->link;
+                    double c = it->weight;
 
-                    // obter o vértice adjacente e o custo da aresta
+                    if (v != destiny && weights[v] > weights[u] + c) {
+                        weights[v] = weights[u] + c;
+                        aux = new NodeWeight(v, weights[v]);
+                        h->heap_insert(aux);
+
+                    } else {
+                        precursor[index] = destiny;
+                        return precursor;
+                    }
+
+                }
+
+                precursor[index] = h->heap_extract()->link;
+                index++;
+
             }
 
         }
@@ -63,9 +159,6 @@ struct Graph {
 
 };
 
-struct MinHeap {
-
-};
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(false);
@@ -91,16 +184,16 @@ int main(int argc, char *argv[]) {
 
     }
 
-    while(cin.eof()) {
+    while(cin) {
         cin >> origin >> destiny;
 
-        int *shortest_path;
-        shortest_path = g.dijkstra(origin, destiny);
+        int *precursor;
+        precursor = g.dijkstra(origin, destiny);
 
-        for(int i = 0; i < (sizeof(shortest_path) / sizeof(*shortest_path)); ++i) { // prescisa ajeitar
-            cout << shortest_path[i];
+        for(int i = 0; precursor[i] != -1; ++i) {
+            cout << precursor[i];
 
-            if(i < sizeof(shortest_path) / sizeof(*shortest_path) - 1) { // prescisa ajeitar
+            if(precursor[i+1] != - 1) {
                 cout << " ";
             } else {
                 cout << endl;
