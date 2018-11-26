42cb500e00c719e27db05c958cd29a6e8ee93e68
Douglas Ventura <dvsf@cin.ufpe.br>
2018-11-26 20:01:16 -0300

    Falta implementar branch_bound

--- Lista07/lemmings.cpp
+++ Lista07/lemmings.cpp
@@ -4,29 +4,38 @@
 
 using namespace std;
 
+string branch_bound(int* inputLemmings[], int* outputLemmings[], int begin, int end) {
+
+}
+
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int f, numLemmings, lemmings;
+    string solution;
 
     cin >> f;
 
     for (int i = 0; i < f; ++i) {
         cin >> numLemmings;
 
-        queue<int> inputLemmings;
-        queue<int> outputLemmings;
+        int* inputLemmings = new int(numLemmings);
+        int* outputLemmings = new int(numLemmings);
 
         for (int j = 0; j < numLemmings; ++j) {
             cin >> lemmings;
-            inputLemmings.push(lemmings);
+            inputLemmings[i] = lemmings;
         }
 
         for (int k = 0; k < numLemmings; ++k) {
             cin >> lemmings;
-            outputLemmings.push(lemmings);
+            outputLemmings[k] = lemmings;
         }
+    
+    solution = branch_bound(&inputLemmings, &outputLemmings, 0, numLemmings - 1);
+
+    cout << solution << endl;
 
     }
 
